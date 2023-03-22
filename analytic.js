18f4cf305b81d7ca1dbb1e00d1612534821c4031
nkaranjit <nkaranjit@gmail.com>
2023-03-21 23:24:54 -0400

    Create analytic.js

---
+++ analytic.js
@@ -0,0 +1,7 @@
+<script type="text/javascript"> 
+(function(n,t,a,e){var i="aptrinsic";n[i]=n[i]||function(){  
+   (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e; 
+   var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
+   var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c) 
+ })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-PRODUCT-KEY-2"); 
+</script> 
